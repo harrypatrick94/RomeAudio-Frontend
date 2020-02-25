6980a6df3c3755fc5c1ec7457b7b616fa41b482b
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-26 09:15:35 +1100

    Updates

---
+++ precache-manifest.d42cb899f9a93efebf68a324bd5b39cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a773c2e2c6e9ccee9c25b45a5d571458",
+    "url": "/harrypatrick94/RomeAudio-Frontend/index.html"
+  },
+  {
+    "revision": "c1011309d5107ffa457e",
+    "url": "/harrypatrick94/RomeAudio-Frontend/static/css/main.ec338a6c.chunk.css"
+  },
+  {
+    "revision": "6907dc7ddb8bf2a8f467",
+    "url": "/harrypatrick94/RomeAudio-Frontend/static/js/2.77513292.chunk.js"
+  },
+  {
+    "revision": "b7ca73b6f678164ca76e42cc6b6448db",
+    "url": "/harrypatrick94/RomeAudio-Frontend/static/js/2.77513292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1011309d5107ffa457e",
+    "url": "/harrypatrick94/RomeAudio-Frontend/static/js/main.36274408.chunk.js"
+  },
+  {
+    "revision": "0e7365ad1cd500f67fa6",
+    "url": "/harrypatrick94/RomeAudio-Frontend/static/js/runtime-main.3830e23f.js"
+  }
+]);
