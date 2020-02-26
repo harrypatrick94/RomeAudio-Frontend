b026fd7409ad8259d7a32ff0d91b5e3c8614faf2
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-26 11:07:49 +1100

    cors fix try

--- src/lib/ajax.js
+++ src/lib/ajax.js
@@ -19,7 +19,7 @@ export default {
   },
   signIn(email, password){
     const url = `${BASE_URL}/signin`
-    return axios.post(url, {
+    return axios.post(url, res.header("Access-Control-Allow-Origin", '*'), {
       email, password
     })
   },
