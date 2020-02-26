f729888a06fda1f72bfa40eb941727477816bb88
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-26 13:10:53 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RomeAudio-Frontend/precache-manifest.1823adf8ecd37980149ce1e5159ea05a.js"
+  "/RomeAudio-Frontend/precache-manifest.4c9ff718a57656b49a963ce3b1b45490.js"
 );
 
 self.addEventListener('message', (event) => {
