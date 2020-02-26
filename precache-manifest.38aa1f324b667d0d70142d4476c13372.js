54a988a39150628c4fc13b3734786aace1db797a
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-26 14:09:40 +1100

    Updates

---
+++ precache-manifest.38aa1f324b667d0d70142d4476c13372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83797cf5b56137a628d3fd2ef67c7855",
+    "url": "/RomeAudio-Frontend/index.html"
+  },
+  {
+    "revision": "52f22bb4897467c83881",
+    "url": "/RomeAudio-Frontend/static/css/main.8df8ee50.chunk.css"
+  },
+  {
+    "revision": "2d39ed53980564b75686",
+    "url": "/RomeAudio-Frontend/static/js/2.78a8494a.chunk.js"
+  },
+  {
+    "revision": "95703a08d3ea5dd388d5747ff558797f",
+    "url": "/RomeAudio-Frontend/static/js/2.78a8494a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f22bb4897467c83881",
+    "url": "/RomeAudio-Frontend/static/js/main.79b1ecd2.chunk.js"
+  },
+  {
+    "revision": "765ea7617b4ab867bbe1",
+    "url": "/RomeAudio-Frontend/static/js/runtime-main.c3a9ad64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RomeAudio-Frontend/precache-manifest.d4cda455a8e03aec68389410618489c6.js"
+  "/RomeAudio-Frontend/precache-manifest.38aa1f324b667d0d70142d4476c13372.js"
 );
 
 self.addEventListener('message', (event) => {
